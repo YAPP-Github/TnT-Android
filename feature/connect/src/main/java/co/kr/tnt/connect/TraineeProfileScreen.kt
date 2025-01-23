bf39646e39f1c10700517ceaacf1933fdd760be7
SeonJeongk <sjkim5621@naver.com>
2025-01-24 01:24:34 +0900

    [TNT-115] feat: 트레이니 프로필 화면 UI 구현

---
+++ feature/connect/src/main/java/co/kr/tnt/connect/TraineeProfileScreen.kt
@@ -0,0 +1,202 @@
+package co.kr.tnt.connect
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.kr.tnt.designsystem.component.button.TnTBottomButton
+import co.kr.tnt.designsystem.component.image.TnTProfileImage
+import co.kr.tnt.designsystem.component.image.model.ProfileType
+import co.kr.tnt.designsystem.theme.TnTTheme
+import co.kr.tnt.feature.connect.R
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun TraineeProfileScreen(
+    onNextClick: () -> Unit,
+) {
+    // TODO 값 전달받기
+    val profileImage = "https://buly.kr/3j7VVqN"
+    val name = "김회원"
+    val age = "24"
+    val height = "168"
+    val weight = "52"
+    val purpose = "체중 감량, 자세 교정"
+    val caution = "발목이 안좋아서 발목에 무리가는 행동을 하면 안 돼요. 잘 부탁드려요!"
+
+    Scaffold { innerPadding ->
+        Image(
+            painter = painterResource(R.drawable.img_connection_complete_background_3x),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+        )
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 40.dp, vertical = 24.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.trainee_who_will_be_with),
+                    style = TnTTheme.typography.h4,
+                    color = TnTTheme.colors.commonColors.Common0,
+                )
+                Spacer(Modifier.height(12.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(20.dp))
+                        .background(TnTTheme.colors.commonColors.Common0)
+                        .padding(horizontal = 20.dp, vertical = 32.dp),
+                ) {
+                    val painter = profileImage?.let { rememberAsyncImagePainter(it) }
+                    TnTProfileImage(
+                        type = ProfileType.Trainee,
+                        image = painter,
+                        imageSize = 128.dp,
+                        showEditButton = false,
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                    Spacer(Modifier.height(12.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Text(
+                            text = name,
+                            style = TnTTheme.typography.h2,
+                            color = TnTTheme.colors.neutralColors.Neutral950,
+                        )
+                        Spacer(Modifier.width(4.dp))
+                        Text(
+                            text = stringResource(R.string.trainee),
+                            style = TnTTheme.typography.h4,
+                            color = TnTTheme.colors.neutralColors.Neutral950,
+                        )
+                    }
+                    Spacer(Modifier.height(32.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        TextWithLabel(
+                            label = stringResource(R.string.age_label),
+                            text = age + stringResource(R.string.age_unit),
+                            modifier = Modifier.weight(1f),
+                        )
+                        TextWithLabel(
+                            label = stringResource(R.string.height_label),
+                            text = height + stringResource(R.string.height_unit),
+                            modifier = Modifier.weight(1f),
+                        )
+                        TextWithLabel(
+                            label = stringResource(R.string.weight_label),
+                            text = weight + stringResource(R.string.weight_unit),
+                            modifier = Modifier.weight(1f),
+                        )
+                    }
+                    Spacer(Modifier.height(32.dp))
+                    TextWithBackground(
+                        label = stringResource(R.string.purpose_of_pt),
+                        text = purpose,
+                    )
+                    Spacer(Modifier.height(32.dp))
+                    TextWithBackground(
+                        label = stringResource(R.string.caution),
+                        text = caution,
+                        modifier = Modifier.height(128.dp),
+                    )
+                }
+            }
+            TnTBottomButton(
+                text = stringResource(R.string.start),
+                // TODO 홈으로 이동
+                onClick = onNextClick,
+                modifier = Modifier.align(Alignment.BottomCenter),
+            )
+        }
+    }
+}
+
+@Composable
+private fun TextWithLabel(
+    label: String,
+    text: String,
+    modifier: Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        Text(
+            text = label,
+            style = TnTTheme.typography.body1Bold,
+            color = TnTTheme.colors.neutralColors.Neutral950,
+        )
+        Spacer(Modifier.width(12.dp))
+        Text(
+            text = text,
+            style = TnTTheme.typography.body2Medium,
+            color = TnTTheme.colors.neutralColors.Neutral500,
+        )
+    }
+}
+
+@Composable
+private fun TextWithBackground(
+    label: String,
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Column {
+        Text(
+            text = label,
+            style = TnTTheme.typography.body1Bold,
+            color = TnTTheme.colors.neutralColors.Neutral900,
+        )
+        Spacer(Modifier.height(7.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(8.dp))
+                .background(TnTTheme.colors.neutralColors.Neutral100)
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+        ) {
+            Text(
+                text = text,
+                style = TnTTheme.typography.label1Medium,
+                color = TnTTheme.colors.neutralColors.Neutral800,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TraineeProfileScreenPreview() {
+    TnTTheme {
+        TraineeProfileScreen(
+            onNextClick = {},
+        )
+    }
+}
