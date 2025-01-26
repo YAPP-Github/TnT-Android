fed8d5110ca6b565edc2cb2115ee7ad1e54fa32f
SeonJeongk <sjkim5621@naver.com>
2025-01-26 12:43:36 +0900

    [TNT-115] fix: domain UserType 기본 정보 가지도록 수정

--- domain/src/main/java/co/kr/tnt/domain/model/UserType.kt
+++ domain/src/main/java/co/kr/tnt/domain/model/UserType.kt
@@ -1,6 +1,24 @@
 package co.kr.tnt.domain.model
 
-enum class UserType {
-    Trainer,
-    Trainee,
+sealed class UserType {
+    abstract val id: String
+    abstract val name: String
+    abstract val image: String?
+
+    data class Trainer(
+        override val id: String,
+        override val name: String,
+        override val image: String? = null,
+    ) : UserType()
+
+    data class Trainee(
+        override val id: String,
+        override val name: String,
+        override val image: String? = null,
+        val age: Int,
+        val weight: Float,
+        val height: Int,
+        val ptPurpose: List<String>,
+        val caution: String? = null,
+    ) : UserType()
 }
