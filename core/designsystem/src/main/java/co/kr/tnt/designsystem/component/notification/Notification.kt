171679331703c8e7bfa12793b34724dc4e53f682
SeonJeongk <sjkim5621@naver.com>
2025-02-01 16:51:20 +0900

    [TNT-189] feat: Notification 컴포넌트 구현

---
+++ core/designsystem/src/main/java/co/kr/tnt/designsystem/component/notification/Notification.kt
@@ -0,0 +1,88 @@
+package co.kr.tnt.designsystem.component.notification
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.kr.tnt.designsystem.component.notification.model.NotificationType
+import co.kr.tnt.designsystem.theme.TnTTheme
+
+@Composable
+fun TnTNotification(
+    type: NotificationType,
+    title: String,
+    contents: String,
+    time: String,
+    isChecked: Boolean,
+) {
+    val backgroundColor = if (isChecked) {
+        TnTTheme.colors.commonColors.Common0
+    } else {
+        TnTTheme.colors.neutralColors.Neutral100
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor)
+            .padding(20.dp),
+    ) {
+        Image(
+            painter = painterResource(type.icon),
+            contentDescription = null,
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 6.dp),
+            ) {
+                Text(
+                    text = title,
+                    color = TnTTheme.colors.neutralColors.Neutral400,
+                    style = TnTTheme.typography.label1Bold,
+                )
+                Text(
+                    text = time,
+                    color = TnTTheme.colors.neutralColors.Neutral400,
+                    style = TnTTheme.typography.label1Medium,
+                )
+            }
+            Text(
+                text = contents,
+                color = TnTTheme.colors.neutralColors.Neutral900,
+                style = TnTTheme.typography.body2Medium,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TnTNotificationPreview() {
+    TnTTheme {
+        TnTNotification(
+            type = NotificationType.LINK,
+            title = "알림 문구",
+            contents = "알림 상세 문구",
+            time = "2분전",
+            isChecked = false,
+        )
+    }
+}
---
+++ core/designsystem/src/main/java/co/kr/tnt/designsystem/component/notification/model/NotificationType.kt
@@ -0,0 +1,8 @@
+package co.kr.tnt.designsystem.component.notification.model
+
+import co.kr.tnt.core.designsystem.R
+
+enum class NotificationType(val icon: Int) {
+    LINK(R.drawable.ic_link_notif),
+    SCHEDULE(R.drawable.ic_schedule_notif),
+}
