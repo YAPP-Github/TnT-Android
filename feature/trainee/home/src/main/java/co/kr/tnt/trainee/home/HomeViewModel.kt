d1337961b522d81dc7180446e6ed11a88c94365f
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2025-02-02 17:15:24 +0900

    [TNT-176] chore: feature:trainee:home 모듈 최초 생성

---
+++ feature/trainee/home/src/main/java/co/kr/tnt/trainee/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package co.kr.tnt.trainee.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel()
