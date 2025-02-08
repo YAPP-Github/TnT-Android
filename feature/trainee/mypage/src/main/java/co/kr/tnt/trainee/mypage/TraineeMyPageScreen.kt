b79488077f0ec4b0854b4bedc7473c462a9b87ed
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2025-02-09 00:56:25 +0900

    [TNT-183] refactor: TnTMyPageButton 내 후행 컴포넌트를 추가할 수 있도록 개선

--- feature/trainee/mypage/src/main/java/co/kr/tnt/trainee/mypage/TraineeMyPageScreen.kt
+++ feature/trainee/mypage/src/main/java/co/kr/tnt/trainee/mypage/TraineeMyPageScreen.kt
@@ -2,7 +2,6 @@ package co.kr.tnt.trainee.mypage
 
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,7 +27,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import co.kr.tnt.core.ui.R
 import co.kr.tnt.designsystem.component.TnTIconPopupDialog
 import co.kr.tnt.designsystem.component.TnTProfileImage
 import co.kr.tnt.designsystem.component.TnTSingleButtonPopupDialog
@@ -37,12 +35,14 @@ import co.kr.tnt.designsystem.component.button.TnTTextButton
 import co.kr.tnt.designsystem.component.button.model.ButtonSize
 import co.kr.tnt.designsystem.component.button.model.ButtonType
 import co.kr.tnt.designsystem.theme.TnTTheme
+import co.kr.tnt.feature.trainee.mypage.R
 import co.kr.tnt.trainee.mypage.TraineeMyPageContract.TraineeMyPageEffect
 import co.kr.tnt.trainee.mypage.TraineeMyPageContract.TraineeMyPageUiEvent
 import co.kr.tnt.trainee.mypage.TraineeMyPageContract.TraineeMyPageUiState
 import co.kr.tnt.trainee.mypage.model.DialogState
 import co.kr.tnt.ui.component.TnTMyPageButton
 import co.kr.tnt.ui.model.DefaultUserProfile
+import co.kr.tnt.core.ui.R as coreR
 
 @Composable
 internal fun TraineeMyPageRoute(
@@ -125,7 +125,7 @@ private fun TraineeMyPageScreen(
             )
             Spacer(Modifier.height(8.dp))
             TnTTextButton(
-                text = stringResource(R.string.modifying_personal_info),
+                text = stringResource(coreR.string.modifying_personal_info),
                 size = ButtonSize.Small,
                 type = ButtonType.Gray,
                 onClick = onEditButtonClick,
@@ -138,30 +138,23 @@ private fun TraineeMyPageScreen(
             ) {
                 if (state.isConnected.not()) {
                     TnTMyPageButton(
-                        text = stringResource(co.kr.tnt.feature.trainee.mypage.R.string.connect_with_trainer),
+                        text = stringResource(R.string.connect_with_trainer),
                         onClick = onConnectButtonClick,
                         verticalPadding = 14.dp,
                     )
                 }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(TnTTheme.colors.commonColors.Common0)
-                        .padding(horizontal = 20.dp, vertical = 12.dp),
-                ) {
-                    Text(
-                        text = stringResource(R.string.app_push_notification),
-                        color = TnTTheme.colors.neutralColors.Neutral700,
-                        style = TnTTheme.typography.body2Medium,
-                    )
-                    TnTSwitch(
-                        checked = state.isPushEnabled,
-                        onClick = onPushNotificationToggle,
-                    )
-                }
+                TnTMyPageButton(
+                    text = stringResource(coreR.string.app_push_notification),
+                    verticalPadding = 12.dp,
+                    enabled = false,
+                    onClick = { },
+                    trailingComponent = {
+                        TnTSwitch(
+                            checked = state.isPushEnabled,
+                            onClick = onPushNotificationToggle,
+                        )
+                    },
+                )
                 Column(
                     modifier = Modifier
                         .clip(RoundedCornerShape(12.dp))
@@ -169,12 +162,12 @@ private fun TraineeMyPageScreen(
                         .padding(vertical = 12.dp),
                 ) {
                     TnTMyPageButton(
-                        text = stringResource(R.string.terms_of_service),
+                        text = stringResource(coreR.string.terms_of_service),
                         onClick = onServiceTermClick,
                         verticalPadding = 8.dp,
                     )
                     TnTMyPageButton(
-                        text = stringResource(R.string.privacy_policy),
+                        text = stringResource(coreR.string.privacy_policy),
                         onClick = onPrivacyClick,
                         verticalPadding = 8.dp,
                     )
@@ -186,7 +179,7 @@ private fun TraineeMyPageScreen(
                             .padding(horizontal = 20.dp, vertical = 8.dp),
                     ) {
                         Text(
-                            text = stringResource(R.string.app_version),
+                            text = stringResource(coreR.string.app_version),
                             color = TnTTheme.colors.neutralColors.Neutral700,
                             style = TnTTheme.typography.body2Medium,
                         )
@@ -197,14 +190,14 @@ private fun TraineeMyPageScreen(
                         )
                     }
                     TnTMyPageButton(
-                        text = stringResource(R.string.open_source_license),
+                        text = stringResource(coreR.string.open_source_license),
                         onClick = onOpenSourceClick,
                         verticalPadding = 8.dp,
                     )
                 }
                 if (state.isConnected) {
                     TnTMyPageButton(
-                        text = stringResource(co.kr.tnt.feature.trainee.mypage.R.string.disconnect_with_trainer),
+                        text = stringResource(R.string.disconnect_with_trainer),
                         onClick = onDisconnectButtonClick,
                         verticalPadding = 14.dp,
                     )
@@ -216,21 +209,15 @@ private fun TraineeMyPageScreen(
                         .background(TnTTheme.colors.commonColors.Common0)
                         .padding(vertical = 12.dp),
                 ) {
-                    Text(
-                        text = stringResource(R.string.logout),
-                        color = TnTTheme.colors.neutralColors.Neutral700,
-                        style = TnTTheme.typography.body2Medium,
-                        modifier = Modifier
-                            .padding(horizontal = 20.dp, vertical = 8.dp)
-                            .clickable(onClick = onLogoutClick),
+                    TnTMyPageButton(
+                        text = stringResource(coreR.string.logout),
+                        onClick = onLogoutClick,
+                        verticalPadding = 8.dp,
                     )
-                    Text(
-                        text = stringResource(R.string.delete_account),
-                        color = TnTTheme.colors.neutralColors.Neutral700,
-                        style = TnTTheme.typography.body2Medium,
-                        modifier = Modifier
-                            .padding(horizontal = 20.dp, vertical = 8.dp)
-                            .clickable(onClick = onDeleteAccountClick),
+                    TnTMyPageButton(
+                        text = stringResource(coreR.string.delete_account),
+                        onClick = onDeleteAccountClick,
+                        verticalPadding = 8.dp,
                     )
                 }
             }
@@ -245,8 +232,8 @@ private fun TraineeMyPageScreen(
                 stringResource(state.dialogState.warningDialogTitle)
             },
             content = stringResource(state.dialogState.warningDialogContent),
-            leftButtonText = stringResource(R.string.cancel),
-            rightButtonText = stringResource(R.string.ok),
+            leftButtonText = stringResource(coreR.string.cancel),
+            rightButtonText = stringResource(coreR.string.ok),
             onLeftButtonClick = onDismissPopup,
             onRightButtonClick = onConfirmWarningDialog,
             onDismiss = onDismissPopup,
@@ -261,7 +248,7 @@ private fun TraineeMyPageScreen(
                 stringResource(state.dialogState.completeDialogTitle)
             },
             content = stringResource(state.dialogState.completeDialogContent),
-            buttonText = stringResource(R.string.ok),
+            buttonText = stringResource(coreR.string.ok),
             onButtonClick = onConfirmCompleteDialog,
             onDismiss = onConfirmCompleteDialog,
         )
