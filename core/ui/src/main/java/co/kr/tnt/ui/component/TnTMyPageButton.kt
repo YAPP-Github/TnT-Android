3ccac7d6dd2fc537ec5754ec8aaa708810de9bc0
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2025-02-09 00:56:25 +0900

    [TNT-183] refactor: TnTMyPageButton 내 후행 컴포넌트를 추가할 수 있도록 개선

--- core/ui/src/main/java/co/kr/tnt/ui/component/TnTMyPageButton.kt
+++ core/ui/src/main/java/co/kr/tnt/ui/component/TnTMyPageButton.kt
@@ -1,8 +1,12 @@
 package co.kr.tnt.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -10,24 +14,29 @@ import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import co.kr.tnt.designsystem.component.TnTSwitch
 import co.kr.tnt.designsystem.theme.TnTTheme
 
 @Composable
 fun TnTMyPageButton(
     text: String,
     verticalPadding: Dp,
+    onClick: () -> Unit = { },
     modifier: Modifier = Modifier,
-    onClick: () -> Unit,
+    enabled: Boolean = true,
+    trailingComponent: (@Composable () -> Unit)? = null,
 ) {
     CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 39.dp) {
         Button(
             onClick = onClick,
             shape = RoundedCornerShape(12.dp),
+            enabled = enabled,
             colors = ButtonColors(
                 containerColor = TnTTheme.colors.commonColors.Common0,
                 contentColor = TnTTheme.colors.neutralColors.Neutral700,
@@ -40,13 +49,23 @@ fun TnTMyPageButton(
                 .fillMaxWidth()
                 .defaultMinSize(Dp.Hairline),
         ) {
-            Text(
-                text = text,
-                style = TnTTheme.typography.body2Medium,
+            Row(
                 modifier = Modifier.fillMaxWidth(),
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1,
-            )
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = text,
+                    style = TnTTheme.typography.body2Medium,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+                trailingComponent?.let { trailingComponent ->
+                    Spacer(modifier = Modifier.width(4.dp))
+                    trailingComponent()
+                }
+            }
         }
     }
 }
@@ -60,6 +79,21 @@ private fun TnTMyPageButtonPreview() {
             text = "트레이너와 연결하기",
             onClick = {},
             verticalPadding = 8.dp,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TnTMyPageButtonWithTrailingComponentPreview() {
+    TnTTheme {
+        TnTMyPageButton(
+            text = "앱 푸시 알림",
+            onClick = {},
+            verticalPadding = 8.dp,
+            trailingComponent = {
+                TnTSwitch(checked = true, onClick = { })
+            },
         )
     }
 }
