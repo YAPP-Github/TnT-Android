73e1705d3c4ba71fda9f12694378a2d7176267b1
SeonJeongk <sjkim5621@naver.com>
2025-02-09 01:12:24 +0900

    [TNT-181] feat: 트레이니 홈화면 기록 관련 data class 구현

---
+++ domain/src/main/java/co/kr/tnt/domain/model/trainee/DailyDataStatus.kt
@@ -0,0 +1,7 @@
+package co.kr.tnt.domain.model.trainee
+
+import java.time.LocalDate
+
+data class DailyDataStatus(
+    val date: List<LocalDate>,
+)
---
+++ domain/src/main/java/co/kr/tnt/domain/model/trainee/TraineeDailyLog.kt
@@ -0,0 +1,30 @@
+package co.kr.tnt.domain.model.trainee
+
+import co.kr.tnt.domain.model.RecordType
+import java.time.LocalDate
+
+data class TraineeDailyLog(
+    val date: LocalDate,
+    val ptSession: PtSession?,
+    val record: List<DailyRecord>?,
+)
+
+data class PtSession(
+    val ptLessonId: String,
+    val trainerName: String,
+    val trainerImage: String,
+    val session: Int,
+    val startTime: String,
+    val endTime: String,
+    val hasRecord: Boolean,
+)
+
+// TODO: RecordType 매핑
+data class DailyRecord(
+    val recordId: String,
+    val recordType: RecordType,
+    val recordTime: String,
+    val recordImage: String?,
+    val recordContents: String,
+    val feedbackCount: Int,
+)
