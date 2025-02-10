48f566b191b0c96dfc8e078a9f380b6c015f1444
SeonJeongk <sjkim5621@naver.com>
2025-02-10 23:03:00 +0900

    [TNT-186] fix: 회원 조회 API 수정 사항 반영

--- data/network/src/main/java/co/kr/data/network/model/UserResponse.kt
+++ data/network/src/main/java/co/kr/data/network/model/UserResponse.kt
@@ -10,17 +10,27 @@ data class UserResponse(
     val name: String,
     val email: String,
     val profileImageUrl: String,
-    val birthday: String?,
     val memberType: MemberType,
     val socialType: String,
+    val trainer: TrainerResponse?,
+    val trainee: TraineeResponse?,
+)
+
+@Serializable
+data class TrainerResponse(
     val activeTraineeCount: Int?,
     val totalTraineeCount: Int?,
     val invitationCode: String?,
-    val trainerId: String?,
+)
+
+@Serializable
+data class TraineeResponse(
+    val birthday: String?,
+    val age: Int?,
     val height: Double?,
     val weight: Double?,
     val cautionNote: String?,
-    val goalContents: List<String>,
+    val ptGoals: List<String>,
 )
 
 fun UserResponse.toDomain(dateFormatter: DateFormatter): User {
@@ -35,12 +45,12 @@ fun UserResponse.toDomain(dateFormatter: DateFormatter): User {
             id = "TODO",
             name = name,
             image = profileImageUrl,
-            birthday = birthday?.let(dateFormatter::parse),
-            weight = requireNotNull(weight),
-            height = requireNotNull(height?.toInt()),
-            ptPurpose = goalContents,
-            caution = cautionNote,
-            isConnected = trainerId != null,
+            birthday = trainee?.birthday?.let(dateFormatter::parse),
+            weight = requireNotNull(trainee?.weight),
+            height = requireNotNull(trainee?.height?.toInt()),
+            ptPurpose = trainee?.ptGoals ?: emptyList(),
+            caution = trainee?.cautionNote,
+            isConnected = false,
         )
 
         MemberType.UNREGISTERED -> error("등록되지 않은 유저입니다.")
