91252ad8b96e6a71c44e2ceec2f11c44853b0b41
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2025-02-18 16:08:32 +0900

    [TNT-000] fix: 카카오톡이 설치되어 있으나 로그인되어 있지 않은 경우 오류가 발생하지 않도록 수정

--- feature/login/src/main/java/co/kr/tnt/login/LoginScreen.kt
+++ feature/login/src/main/java/co/kr/tnt/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package co.kr.tnt.login
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -91,6 +92,7 @@ internal fun LoginRoute(
                         )
                     }
                     .onFailure { throwable ->
+                        Log.e("TnT Login error", throwable.toString())
                         viewModel.setEvent(LoginUiEvent.OnAuthFail(throwable))
                     }
             }
