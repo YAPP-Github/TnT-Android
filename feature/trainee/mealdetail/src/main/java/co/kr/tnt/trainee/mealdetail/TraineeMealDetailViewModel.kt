52b4f6529847f80497e72beff2b4bd61cb2724f3
SeonJeongk <sjkim5621@naver.com>
2025-03-28 13:07:40 +0900

    [TNT-269] feat: 식단 기록 상세 화면 및 기능 구현

--- feature/trainee/mealdetail/src/main/java/co/kr/tnt/trainee/mealdetail/TraineeMealDetailViewModel.kt
+++ feature/trainee/mealdetail/src/main/java/co/kr/tnt/trainee/mealdetail/TraineeMealDetailViewModel.kt
@@ -1,16 +1,54 @@
 package co.kr.tnt.trainee.mealdetail
 
+import androidx.lifecycle.viewModelScope
+import co.kr.tnt.domain.model.RecordType.MealType
+import co.kr.tnt.domain.repository.TraineeRepository
 import co.kr.tnt.trainee.mealdetail.TraineeMealDetailContract.TraineeMealDetailSideEffect
 import co.kr.tnt.trainee.mealdetail.TraineeMealDetailContract.TraineeMealDetailUiEvent
 import co.kr.tnt.trainee.mealdetail.TraineeMealDetailContract.TraineeMealDetailUiState
 import co.kr.tnt.ui.base.BaseViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-internal class TraineeMealDetailViewModel @Inject constructor() :
+internal class TraineeMealDetailViewModel @Inject constructor(
+    private val traineeRepository: TraineeRepository,
+) :
     BaseViewModel<TraineeMealDetailUiState, TraineeMealDetailUiEvent, TraineeMealDetailSideEffect>(
-        TraineeMealDetailUiState(),
-    ) {
+            TraineeMealDetailUiState(),
+        ) {
         override suspend fun handleEvent(event: TraineeMealDetailUiEvent) {
-            TODO("Not yet implemented")
+            when (event) {
+                is TraineeMealDetailUiEvent.LoadMealDetail -> fetchRecordDetail(event.id)
+                TraineeMealDetailUiEvent.OnClickMore -> {}
+            }
+        }
+
+        private fun fetchRecordDetail(id: Long) {
+            viewModelScope.launch {
+                runCatching {
+                    traineeRepository.getMealRecord(id)
+                }.onSuccess { result ->
+                    updateState {
+                        copy(
+                            image = result.imageUrl,
+                            date = result.date,
+                            mealType = convertToMealType(result.dietType),
+                            memo = result.memo,
+                        )
+                    }
+                }.onFailure {
+                    sendEffect(TraineeMealDetailSideEffect.ShowToast("데이터 불러오기에 실패했어요"))
+                }
+            }
+        }
+
+        private fun convertToMealType(mealType: String): MealType {
+            return when (mealType.uppercase()) {
+                "BREAKFAST" -> MealType.BREAKFAST
+                "LUNCH" -> MealType.LUNCH
+                "DINNER" -> MealType.DINNER
+                "SNACK" -> MealType.SNACK
+                else -> MealType.BREAKFAST
+            }
         }
     }
