c84c3706ceb857bf5491f369fe9a78a79a719a5e
SeonJeongk <sjkim5621@naver.com>
2025-04-03 18:28:31 +0900

    [TNT-272] refactor: TrainerSignUpViewModel 내 Context 제거

--- feature/trainer/signup/src/main/java/co/kr/tnt/trainer/signup/TrainerSignUpContract.kt
+++ feature/trainer/signup/src/main/java/co/kr/tnt/trainer/signup/TrainerSignUpContract.kt
@@ -1,10 +1,10 @@
 package co.kr.tnt.trainer.signup
 
-import android.content.Context
 import android.net.Uri
 import co.kr.tnt.ui.base.UiEvent
 import co.kr.tnt.ui.base.UiSideEffect
 import co.kr.tnt.ui.base.UiState
+import java.io.File
 
 private const val MAX_LENGTH = 15
 
@@ -28,8 +28,7 @@ internal class TrainerSignUpContract {
         data object OnNextClick : TrainerSignUpUiEvent
         data object OnBackClick : TrainerSignUpUiEvent
         data class RequestSignUp(
-            val context: Context,
-            val imageUri: Uri?,
+            val imageFile: File?,
             val id: String,
             val email: String,
             val authType: String,
