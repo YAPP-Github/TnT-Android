6c3b33140968a9c077252e6c33baa0445979cbd2
SeonJeongk <sjkim5621@naver.com>
2025-10-25 14:53:34 +0900

    [TNT-261] feat: 트레이니 개인 정보 수정 화면 진입 시 정보 불러오기

--- feature/trainee/modifymyinfo/src/main/java/co/kr/tnt/trainee/modifymyinfo/TraineeModifyMyInfoViewModel.kt
+++ feature/trainee/modifymyinfo/src/main/java/co/kr/tnt/trainee/modifymyinfo/TraineeModifyMyInfoViewModel.kt
@@ -1,22 +1,33 @@
 package co.kr.tnt.trainee.modifymyinfo
 
+import androidx.lifecycle.viewModelScope
+import co.kr.tnt.core.ui.R.string.core_failed_to_server_request
 import co.kr.tnt.domain.model.ProfileImageUpdatePolicy
+import co.kr.tnt.domain.repository.TraineeRepository
 import co.kr.tnt.trainee.modifymyinfo.TraineeModifyMyInfoContract.TraineeModifyMyInfoEffect
 import co.kr.tnt.trainee.modifymyinfo.TraineeModifyMyInfoContract.TraineeModifyMyInfoUiEvent
 import co.kr.tnt.trainee.modifymyinfo.TraineeModifyMyInfoContract.TraineeModifyMyInfoUiState
 import co.kr.tnt.trainee.modifymyinfo.TraineeModifyMyInfoContract.TraineeModifyMyInfoUiState.DialogState
 import co.kr.tnt.ui.base.BaseViewModel
+import co.kr.tnt.ui.resource.DisplayText
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import java.io.File
 import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
-internal class TraineeModifyMyInfoViewModel @Inject constructor() :
+internal class TraineeModifyMyInfoViewModel @Inject constructor(
+    private val traineeRepository: TraineeRepository,
+) :
     BaseViewModel<TraineeModifyMyInfoUiState, TraineeModifyMyInfoUiEvent, TraineeModifyMyInfoEffect>(
-        TraineeModifyMyInfoUiState(),
-    ) {
+            TraineeModifyMyInfoUiState(),
+        ) {
         private var profileImageUpdatePolicy: ProfileImageUpdatePolicy = ProfileImageUpdatePolicy.Keep
+
+        init {
+            loadUserInfo()
+        }
 
         override suspend fun handleEvent(event: TraineeModifyMyInfoUiEvent) {
             when (event) {
@@ -36,9 +47,36 @@ internal class TraineeModifyMyInfoViewModel @Inject constructor() :
                     updateState { copy(dialogState = DialogState.NONE) }
                     sendEffect(TraineeModifyMyInfoEffect.NavigateToBack)
                 }
+
                 TraineeModifyMyInfoUiEvent.OnDismissDialog -> updateState { copy(dialogState = DialogState.NONE) }
                 TraineeModifyMyInfoUiEvent.OnClickBack -> navigateToBack()
                 TraineeModifyMyInfoUiEvent.OnClickComplete -> updateUserInfo()
+            }
+        }
+
+        private fun loadUserInfo() {
+            viewModelScope.launch {
+                runCatching {
+                    traineeRepository.getMyInfo()
+                }.onSuccess { user ->
+                    updateState {
+                        copy(
+                            profileImage = user.image,
+                            name = user.name,
+                            birthday = user.birthday,
+                            height = user.height?.toString(),
+                            weight = user.weight?.toString(),
+                            ptPurpose = user.ptPurpose,
+                            caution = user.caution,
+                        )
+                    }
+                }.onFailure {
+                    sendEffect(
+                        TraineeModifyMyInfoEffect.ShowToast(
+                            DisplayText.Resource(core_failed_to_server_request),
+                        ),
+                    )
+                }
             }
         }
 
